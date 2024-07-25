3b03ca5c1298b06a1e4f33e8301965fd1c1c4c83
Pousali Chowdhury <136922307+PousaliChowdhury@users.noreply.github.com>
2024-07-25 12:40:30 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,30 @@
+public class SelectionSort {
+    public static void printArray(int[] arr) {
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        // Scanner s = new Scanner(System.in);
+        // int size = s.nextInt();
+        // int arr[] = new int[size];
+        int arr[] = {7,8,3,1,2};
+
+        //Time Complexity = O(n^2)
+        //Selection Sort
+        for(int i=0; i<arr.length-1; i++){
+            int smallest = i;
+            for(int j=i+1; j<arr.length; j++){ // n(n-1)/2 = n^2
+                if(arr[smallest] > arr[j]){
+                    smallest = j;
+                }
+            }
+            //swap
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+        printArray(arr);
+    }
+}
